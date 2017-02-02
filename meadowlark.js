502773caa72594838eb42addcd40a7fdde9ebfdf
James Campbell <james_campbell@my.uri.edu>
2017-02-02 11:14:45 -0500

    Initial commit.

---
+++ meadowlark.js
@@ -0,0 +1,55 @@
+/**
+ * Created by jcampbell on 2/2/17.
+ */
+
+var express = require('express');
+
+/// App Setup
+var app = express();
+
+var handlebars = require('express-handlebars')
+    .create({defaultLayout: 'main', extname: 'hbs'});
+app.engine('hbs', handlebars.engine);
+app.set('view engine', 'hbs');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+];
+
+/// Routes
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.get('/about', function(req, res){
+    var randomFortune =
+        fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', { fortune: randomFortune });
+});
+
+/// Error Handling
+//custom 404 page
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
+
+//custom 500 page
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log( 'Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-C to terminate.' );
+});
