5a94e4d37b6413fa26fdfe46066e1894e0d390b4
James Campbell <james_campbell@my.uri.edu>
2017-02-15 14:26:42 -0500

    Finshed grunt and chapter 5

---
+++ GruntFile.js
@@ -0,0 +1,22 @@
+/**
+ * Created by jcampbell on 2/15/17.
+ */
+
+module.exports = function(grunt){
+    // load plugins
+    [
+        'grunt-cafe-mocha'
+    ].forEach(function(task){
+        grunt.loadNpmTasks(task);
+    });
+
+    // configure plugins
+    grunt.initConfig({
+        cafemocha: {
+            all: {src: 'qa/tests-*.js', options: {ui: 'tdd'},}
+        },
+    });
+
+    // register tasks
+    grunt.registerTask('default', ['cafemocha']);
+};
