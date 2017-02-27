7029cc95cc6526e304ff07e44c0fcca876689c88
James Campbell <james_campbell@my.uri.edu>
2017-02-27 12:32:23 -0500

    Changed newsletter form to AJAX

--- meadowlark.js
+++ meadowlark.js
@@ -72,7 +72,11 @@ app.post('/process', function (req, res) {
     console.log('CSRF token (form hidden form field): ' + req.body._csrf);
     console.log('Name (from visible form field): ' + req.body.name);
     console.log('Email (from visible form field): ' + req.body.email);
-    res.redirect(303, '/thank-you');
+    if(req.xhr || req.accepts('json,html') === 'json') {
+        res.send({success: true});
+    } else {
+        res.redirect(303, '/thank-you');
+    }
 });
 
 app.get('/thank-you', function (req, res) {
