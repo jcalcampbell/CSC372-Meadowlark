ee8f81baf09c086086b54b6636628bbbae74925a
James Campbell <james_campbell@my.uri.edu>
2017-04-03 12:10:13 -0400

    Created vacations database model

---
+++ models/vacation.js
@@ -0,0 +1,26 @@
+/**
+ * Created by jcampbell on 3/27/17.
+ */
+
+var mongoose = require('mongoose');
+
+var vacationSchema = mongoose.Schema({
+    name: String,
+    slug: String,
+    category: String,
+    sku: String,
+    description: String,
+    priceInCents: Number,
+    tags: [String],
+    inSeason: Boolean,
+    available: Boolean,
+    requiresWavier: Boolean,
+    maximumGuests: Number,
+    notes: String,
+    packagesSold: Number,
+});
+vacationSchema.methods.getDisplayPrice = function () {
+    return '$' + (this.priceInCents / 100).toFixed(2);
+};
+var Vacation = mongoose.model('Vacation', vacationSchema);
+module.exports = Vacation;
